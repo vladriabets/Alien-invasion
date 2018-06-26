b7068b3dfe7ad8fb68ea2dd262f21dfe6fcea18d
Vlad <vladriabets@yandex.ru>
2018-06-26 19:21:13 +0300

    ship cannot go through the edges of the screen

--- ship.py
+++ ship.py
@@ -21,10 +21,11 @@ class Ship:
 
     def update(self):
         """Обновляет позицию корабля с учетом флагов."""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
+        # Обновление атрибута rect на основании self.center
         self.rect.centerx = self.center
 
     def blitme(self):
