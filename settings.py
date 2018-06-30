85fea1a1b6b26676774091e0860dc0fb9869b6b6
Vlad <vladriabets@yandex.ru>
2018-06-30 19:51:04 +0300

    only 3 bullets on the screen at the moment

--- settings.py
+++ settings.py
@@ -8,7 +8,9 @@ class Settings:
         self.bg_color = (0, 0, 230)
         # Настройки корабля
         self.ship_speed_factor = 1.5
+        # Параметры пули
         self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
